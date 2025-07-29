d661c7bbfc68ef88b1358eacfeaccba09e423b3b
Adilson Carvalho <145710870+AdilsondeCarvalho@users.noreply.github.com>
2025-07-29 10:48:36 -0300

    29 07 2025

---
+++ REVISÃO/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Excesso de velocidade. Você foi multado!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança.`)
